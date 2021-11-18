a8ae236eec658fef421a472f016b34e284e49f33
PraveenRanguu <prangu@innominds.com>
2021-11-18 13:18:38 +0530

    changes in ui and added states and handlers

--- mycalci/src/app/MyComponents/main-body/main-body.component.ts
+++ mycalci/src/app/MyComponents/main-body/main-body.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main-body',
   templateUrl: './main-body.component.html',
   styleUrls: ['./main-body.component.css']
 })
-export class MainBodyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainBodyComponent {
+  result:string="";
+  longButtons:string[]= ['AC', 'CE'];
+  buttons:string[]= ['7', '8', '9', '/','4','5','6', '*', '1','2', '3','-','.','0','=', '+' ]
+   
+  private previosValue:string="";
+  private currentValue:string="";
+  /**
+   * function
+   * @param value 
+   */
+  addToExpression(value:string){
+    if(this.result!=""){
+      this.previosValue = this.currentValue;
+      this.currentValue = value;
+    }
+    if(value=="AC"){
+       this.result="";
+    }else if(value == "CE"){
+       this.result = this.previosValue != "=" ? this.result.slice(0,-1): this.result;
+    }else if(value == "="){
+           this.result = eval(this.result)
+    }else{
+      this.result += value;
+    }
   }
-
 }
